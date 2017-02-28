228cd480dcd80f8fa09273aa0b71ad8b46358647
lahirud <d.lahiru@mubasher.net>
2017-02-28 11:57:24 +0530

    Initial websocket poc

---
+++ POC/Websocket POC/src/app/websocket/queue-mannager.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { QueueMannagerService } from './queue-mannager.service';
+
+describe('QueueMannagerService', () => {
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [QueueMannagerService]
+		});
+	});
+
+	it('should ...', inject([QueueMannagerService], (service: QueueMannagerService) => {
+		expect(service).toBeTruthy();
+	}));
+});
---
+++ POC/Websocket POC/src/app/websocket/websocket.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import * as Rx from 'rxjs/Rx';
+import { PulseService } from './pulse.service';
+
+@Injectable()
+export class WebsocketService {
+	public ws;
+	public pulseGenerator;
+	private socket: Rx.Subject<MessageEvent>;
+	constructor() { }
+
+	public connect(conOb): Rx.Subject<MessageEvent> {
+		this.socket = this.create(conOb.url);
+		console.log('Connecting...');
+		return this.socket;
+	}
+
+	private create(url): Rx.Subject<MessageEvent> {
+		this.ws = new WebSocket(url);
+
+		const observable = Rx.Observable.create(
+			(obs: Rx.Observer<MessageEvent>) => {
+				this.ws.onopen = (evt) => {
+					this.pulseGenerator = new PulseService();
+					this.pulseGenerator.sendPulse(this);
+					console.log('Connected: ' + url);
+				};
+				this.ws.onmessage = obs.next.bind(obs);
+				this.ws.onerror = obs.error.bind(obs);
+				this.ws.onclose = obs.complete.bind(obs);
+				return this.ws.close.bind(this.ws);
+		});
+
+		const observer = {
+			next: (data: Object) => {
+				if (this.ws.readyState === WebSocket.OPEN) {
+					this.ws.send(JSON.stringify(data));
+				}
+			}
+		};
+
+		return Rx.Subject.create(observer, observable);
+	}
+
+}
