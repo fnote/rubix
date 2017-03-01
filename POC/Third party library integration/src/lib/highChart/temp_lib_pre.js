099d99775c42eebe724b5138502c4ab2087d1727
Amila Manathunga <amila@Amilas-MacBook-Pro.local>
2017-03-01 10:33:39 +0530

    Removed old entries

--- POC/Third party library integration/src/lib/highChart/temp_lib_pre.js
+++
@@ -1,6 +0,0 @@
-(function($){
-    var highChartLib = {
-        location: "lib_high.js"
-    };
-    console.log(App.util.formatters.formatDateForPattern(Date.now(), 'y/M/dd h:m:s.S ')+' : loading '+highChartLib.location);
-})(jQuery);
