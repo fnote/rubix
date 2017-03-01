afeb7513ecc5feaa7e23d5821748f4dc2de8259d
Amila Manathunga <amila@Amilas-MacBook-Pro.local>
2017-03-01 12:50:56 +0530

    Code refactored and reviewed

--- POC/websocket-poc/src/app/app.module.ts
+++ POC/websocket-poc/src/app/app.module.ts
@@ -3,22 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { WebsocketService } from './websocket/websocket.service';
-import {QueueMannagerService} from './websocket/queue-mannager.service';
+import { QueueMannagerService } from './websocket/queue-mannager.service';
 import { DataService } from './data.service';
-
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-	declarations: [
-	AppComponent
-	],
+	declarations: [AppComponent],
 	imports: [
 		BrowserModule,
 		FormsModule,
 		HttpModule
 	],
-	providers: [WebsocketService, QueueMannagerService, DataService],
+	providers: [
+		WebsocketService,
+		QueueMannagerService,
+		DataService
+		],
 	bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- POC/websocket-poc/src/app/websocket/pulse.service.ts
+++ POC/websocket-poc/src/app/websocket/pulse.service.ts
@@ -3,25 +3,29 @@ import { WebsocketService } from './websocket.service';
 
 @Injectable()
 export class PulseService {
-	public pulseInterval; missedPulses = 0;
+	private pulseInterval; 
+	private missedPulses = 0;
 	constructor() { }
 
 	public sendPulse(wsService: WebsocketService) {
 		const ws = wsService.ws;
 		this.pulseInterval = setInterval(() => {
 		this.missedPulses++;
+		
 		if (this.missedPulses >= 5) {
 			ws.close();
 			clearInterval(this.pulseInterval);
 			console.log('Connection Disconnected.');
 			return;
 		}
+
 		ws.send(this.missedPulses);
 		console.log('Pulse sent: ' + this.missedPulses);
 		}, 5000);
 	}
 
 	public resetPulse(wsService: WebsocketService) {
+		// TODO: [Lahiru] Refactor once the log module is completed
 		console.log('Pulse Resetting...');
 		clearInterval(this.pulseInterval);
 		this.missedPulses = 0;
