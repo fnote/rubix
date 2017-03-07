20b5806e2faaac50dde5f54012a45658c174360e
ChandanaB <b.chandana@mubasher.net>
2017-03-07 14:37:32 +0530

    Add Ajax service module

--- POC/websocket-poc/src/app/app.component.ts
+++ POC/websocket-poc/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestMethod } from '@angular/http';
 import { DataService } from './communication/data.service';
 
 @Component({
@@ -23,11 +24,11 @@ export class AppComponent implements OnInit {
 			index: 1,
 			data: this.textValue
 		};
-		this.dataService.send(sendMessage);
+		this.dataService.sendToWs(sendMessage);
 	}
 
 	unsubscribeChatServer() {
-		this.dataService.unsubscribeConnection(1);
+		this.dataService.unsubscribeWsConnection(1);
 	}
 
 	sendMsgToEchoServer() {
@@ -35,10 +36,49 @@ export class AppComponent implements OnInit {
 			index: 0,
 			data: this.textValue
 		};
-		this.dataService.send(sendMessage);
+		this.dataService.sendToWs(sendMessage);
 	}
 
 	unsubscribeEchoServer() {
-		this.dataService.unsubscribeConnection(0);
+		this.dataService.unsubscribeWsConnection(0);
+	}
+
+	sendAjaxRequest(){
+		const data = {
+				DAT:{
+					EMAIL: "aa@aa.aa",
+					FIRST_NME : "Chandana",
+					LST_NME : "BAndara"
+				},
+				HED: {
+					CHNL_ID:25,
+					CL_IP:"", 
+					CL_REQ_ID: "''",
+					CL_VER:"1.0.2.08",
+					LANG_ID:"EN",
+					MSG_GRP:5,
+					MSG_TYP:9,
+					SESN_ID:"",
+					USR_ID:"",
+					VER:"DFN_JSON_1.0"
+				}
+		};
+		const samplePostReq = {
+			method : RequestMethod.Post,
+			url : 'http://mtplusglobal-uat.mubashertrade.com/tmpassword',
+			data : data
+		}
+		this.dataService.sendAjaxRequest(samplePostReq).then(response=>{
+			console.log(response);
+		});
+
+		const sampleGetReq = {
+			method : RequestMethod.Get,
+			url : 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+		}
+
+		this.dataService.sendAjaxRequest(sampleGetReq).then(response=>{
+			console.log(response);
+		});
 	}
 }
