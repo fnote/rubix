da9bb37aec496502056442e44ba282f2b600d076
Amila Manathunga <amila@Amilas-MacBook-Pro.local>
2017-03-07 12:36:01 +0530

    Code refactored

--- src/app/app-backend/price/business-entities/market-entity.ts
+++ src/app/app-backend/price/business-entities/market-entity.ts
@@ -2,6 +2,14 @@ import { BaseEntity } from './base-entity';
 
 export class MarketEntity extends BaseEntity {
 
+	// Meta
+	exchangeCode : string;
+	code : string;
+
+	// Realtime
+	volume : number;
+	turnover : number;
+
 	constructor(values : Object = {}) {
 		super();
 		Object.assign(this, values);
