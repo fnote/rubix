c9649f1573e7c3a0447be6836c248344a14afce7
lahirud <d.lahiru@mubasher.net>
2017-03-07 15:01:42 +0530

    Ajax service added to the main project

---
+++ src/app/communication/ajax/ajax.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import * as Rx from 'rxjs/Rx';
+import {RequestOptions, Request, Http} from '@angular/http';
+
+@Injectable()
+export class AjaxService {
+
+	constructor(private http: Http) {}
+
+	public send(requestOptions): Promise<any> {
+		const options = new RequestOptions({
+						method: requestOptions.method,
+						url: requestOptions.url,
+						body: requestOptions.data ? requestOptions.data : null ,
+						search: requestOptions.search ? requestOptions.search : null,
+						headers : requestOptions.headers ? requestOptions.headers : null,
+						withCredentials : requestOptions.withCredentials ? requestOptions.withCredentials : false,
+						responseType : requestOptions.responseType ? requestOptions.responseType : null
+					});
+		const request = new Request(options);
+		return this.http.request(request).toPromise();
+	}
+}
+
