a8a7f7b2354ce0a45066adb70963ba182708079f
ChandanaB <b.chandana@mubasher.net>
2017-03-07 12:25:46 +0530

    Ajax Service added to communication module

--- POC/websocket-poc/src/app/communication/ajax/ajax.service.ts
+++ POC/websocket-poc/src/app/communication/ajax/ajax.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import * as Rx from 'rxjs/Rx';
 
 @Injectable()
 export class AjaxService {
 
-	constructor() { }
+	constructor(private http: Http) { }
 
 }
