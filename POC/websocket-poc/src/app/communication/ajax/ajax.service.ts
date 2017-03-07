e2d693ee6a1124548d36bf855f67f9306d050a9a
lahirud <d.lahiru@mubasher.net>
2017-03-07 15:44:54 +0530

    code refractoring

--- POC/websocket-poc/src/app/communication/ajax/ajax.service.ts
+++ POC/websocket-poc/src/app/communication/ajax/ajax.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import * as Rx from 'rxjs/Rx';
-import {RequestOptions, Request, Http} from '@angular/http';
+import { RequestOptions, Request, Http } from '@angular/http';
 
 @Injectable()
 export class AjaxService {
 
-	constructor(private http: Http) {}
+	constructor(private http: Http) { }
 
 	public send(requestOptions): Promise<any> {
 		const options = new RequestOptions({
@@ -13,9 +13,9 @@ export class AjaxService {
 						url: requestOptions.url,
 						body: requestOptions.data ? requestOptions.data : null ,
 						search: requestOptions.search ? requestOptions.search : null,
-						headers : requestOptions.headers ? requestOptions.headers : null,
-						withCredentials : requestOptions.withCredentials ? requestOptions.withCredentials : false,
-						responseType : requestOptions.responseType ? requestOptions.responseType : null
+						headers: requestOptions.headers ? requestOptions.headers : null,
+						withCredentials: requestOptions.withCredentials ? requestOptions.withCredentials : false,
+						responseType: requestOptions.responseType ? requestOptions.responseType : null
 					});
 		const request = new Request(options);
 		return this.http.request(request).toPromise();
