5b028a768d063954e742eb170c6bbf484314f7cf
Amila Manathunga <amila@amilam-mbp.dtech.local>
2017-03-10 15:09:04 +0530

    COde refactored

--- src/app/app-backend/price/business-entities/market-entity.ts
+++ src/app/app-backend/price/business-entities/market-entity.ts
@@ -2,13 +2,42 @@ import { BaseEntity } from './base-entity';
 
 export class MarketEntity extends BaseEntity {
 
-	// Meta
-	exchangeCode : string;
-	code : string;
+	private _exchangeCode : string = null;
+	private _code : string = null;
+	private _volume : number = null;
+	private _turnover : number = null;
 
-	// Realtime
-	volume : number;
-	turnover : number;
+	public get exchangeCode() : string  {
+		return this._exchangeCode;
+	}
+
+	public set exchangeCode(value : string ) {
+		this._exchangeCode = value;
+	}
+
+	public get code() : string  {
+		return this._code;
+	}
+
+	public set code(value : string ) {
+		this._code = value;
+	}
+
+	public get volume() : number  {
+		return this._volume;
+	}
+
+	public set volume(value : number ) {
+		this._volume = value;
+	}
+
+	public get turnover() : number  {
+		return this._turnover;
+	}
+
+	public set turnover(value : number ) {
+		this._turnover = value;
+	}
 
 	constructor(values : Object = {}) {
 		super();
