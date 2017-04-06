d58086a098f4288852c7bd5978a3aca37ea193a6
MalinduA <a.malindu@mubasher.net>
2017-04-06 12:31:46 +0530

    RUB-31 : Initial widget creation

--- src/app/widgets/price-widgets/price-widget.module.ts
+++ src/app/widgets/price-widgets/price-widget.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { DetailQuoteComponent } from './detail-quote/detail-quote.component';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { TimeAndSalesComponent } from './time-and-sales/time-and-sales.component';
 
 @NgModule({
 	imports: [CommonModule, FormsModule],
 	declarations: [
 		DetailQuoteComponent,
+		TimeAndSalesComponent,
 	],
 })
 export class PriceWidgetModule { }
---
+++ src/app/widgets/price-widgets/time-and-sales/time-and-sales.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-and-sales',
+  templateUrl: './time-and-sales.component.html',
+})
+export class TimeAndSalesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
