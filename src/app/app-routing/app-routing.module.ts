852275bc616b394f90e70c73052b6e25214071a4
chaaminim <m.chaamini@mubasher.net>
2017-04-07 18:16:31 +0530

    Refactor routes

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,14 +1,5 @@
 import { RouterModule, Routes } from '@angular/router';
-import { ChartComponent } from '../widgets/price-widgets/chart/chart.component';
-import { DetailQuoteComponent } from '../widgets/price-widgets/detail-quote/detail-quote.component';
-import { LoginComponent } from '../widgets/common-widgets/login-widget/login.component';
 import { NgModule } from '@angular/core';
-import { OrderBookComponent } from '../widgets/price-widgets/order-book/order-book.component';
-import { PrimaryLayoutOneComponent } from '../layouts/primary-layout-one/primary-layout-one.component';
-import { RubixTestPageComponent } from '../widgets/common-widgets/rubix-test-page/rubix-test-page.component';
-import { SecondaryLayoutOneComponent } from '../layouts/secondary-layout-one/secondary-layout-one.component';
-import { SideBarComponent } from '../widgets/common-widgets/side-bar/side-bar.component';
-import { TimeAndSalesComponent } from '../widgets/price-widgets/time-and-sales/time-and-sales.component';
 import { routes } from './app-routes';
 
 @NgModule({
