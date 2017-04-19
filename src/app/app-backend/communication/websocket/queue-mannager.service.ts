5414410ccf70479627135fe16af48a9a9a7bdf9b
chandanamadu <b.chandana@mubasher.net>
2017-04-19 00:05:13 -0700

    Handle asynchronous behaviour in QueueMannager

--- src/app/app-backend/communication/websocket/queue-mannager.service.ts
+++ src/app/app-backend/communication/websocket/queue-mannager.service.ts
@@ -12,10 +12,13 @@ export class QueueMannagerService {
 	private TIME_INTERVAL = 300; // 1000 * 3
 	private connectedSocketPool: Array<Connection> = [];
 	private response$: Rx.Subject<any> ;
+	private isConfigLoaded = false;
+	private updateConfigurations$: Rx.Subject<boolean> ;
 
 	constructor(private configService: ConfigService, private websocketService: WebsocketService,
 		private loggerService: LoggerService) {
 		this.response$ = new Rx.Subject();
+		this.updateConfigurations$ = new Rx.Subject();
 		this.init();
 	}
 
@@ -40,6 +43,8 @@ export class QueueMannagerService {
 				};
 				this.connectedSocketPool.push(connConfig);
 			});
+			this.updateConfigurations$.next(true);
+			this.isConfigLoaded = true;
 		});
 	}
 
@@ -51,34 +56,43 @@ export class QueueMannagerService {
 		return messageQueue.shift();
 	}
 
-	public getConnectionByChannel(channel: number): Connection {
-		return this.connectedSocketPool[channel];
+	public getConnectionByChannel(channel: number): Promise<Connection> {
+		return new Promise((resolve,reject) => {
+			if(this.isConfigLoaded){
+				resolve(this.connectedSocketPool[channel]);
+			}else{
+				this.updateConfigurations$.subscribe( configLoaded => {
+					resolve(this.connectedSocketPool[channel]);
+				});
+			}
+		});
 	}
 
 	private connect(connectionConfig: Connection): void {
-		const connection: Connection = this.getConnectionByChannel(connectionConfig.channel);
-		if (!connection.isConnected) {
-			this.websocketService.initConnection(connection).then(sockt => {
-				const socket: Rx.Subject<MessageEvent> = this.websocketService.createConnection(sockt);
-				connection.connectedSocket = socket;
-				connection.isConnected = true;
-				this.subscribeForConnected(connectionConfig.channel);
-				this.activateSentReceive(
-					connection.sendMessageQueue,
-					connection.sendQueueProcessInterval,
-					connection.connectedSocket,
-					connection.channel);
-				this.activateSentReceive(
-					connection.recivedMessageQueue,
-					connection.recivedQueueProcessInterval,
-					this.response$ ,
-					connection.channel);
-			}).catch(error => {
-				this.loggerService.logInfo('error occured ' + connectionConfig.channel , 'QueueMannagerService');
-			});
-		}else {
-			this.loggerService.logInfo('allready connected ' + connectionConfig.channel , 'QueueMannagerService');
-		}
+		this.getConnectionByChannel(connectionConfig.channel).then(connection => {
+			if (!connection.isConnected) {
+				this.websocketService.initConnection(connection).then(sockt => {
+					const socket: Rx.Subject<MessageEvent> = this.websocketService.createConnection(sockt);
+					connection.connectedSocket = socket;
+					connection.isConnected = true;
+					this.subscribeForConnected(connectionConfig.channel);
+					this.activateSentReceive(
+						connection.sendMessageQueue,
+						connection.sendQueueProcessInterval,
+						connection.connectedSocket,
+						connection.channel);
+					this.activateSentReceive(
+						connection.recivedMessageQueue,
+						connection.recivedQueueProcessInterval,
+						this.response$ ,
+						connection.channel);
+				}).catch(error => {
+					this.loggerService.logInfo('error occured ' + connectionConfig.channel , 'QueueMannagerService');
+				});
+			}else {
+				this.loggerService.logInfo('allready connected ' + connectionConfig.channel , 'QueueMannagerService');
+			}
+		});
 	}
 
 	private activateSentReceive(messageQueue: Array<any>, timeIntervalProcess: NodeJS.Timer,
@@ -98,37 +112,40 @@ export class QueueMannagerService {
 	}
 
 	private subscribeForConnected(channel: number): void {
-		const connection: Connection = this.getConnectionByChannel(channel);
-		if (connection.connectedSocket && !connection.subscription) {
-			connection.subscription = connection.connectedSocket.subscribe(msg => {
-				this.enQueueMessage(msg.data, connection.recivedMessageQueue);
-				connection.pulseService.resetPulse();
-			}, error => {
-				this.loggerService.logError(error, 'QueueMannagerService');
-			}, () => {
-				this.unsubcribeConnection(connection.channel);
-				this.loggerService.logInfo('disconnected ' + connection.channel , 'QueueMannagerService');
-			});
-		}
+		this.getConnectionByChannel(channel).then(connection => {
+			if (connection.connectedSocket && !connection.subscription) {
+				connection.subscription = connection.connectedSocket.subscribe(msg => {
+					this.enQueueMessage(msg.data, connection.recivedMessageQueue);
+					connection.pulseService.resetPulse();
+				}, error => {
+					this.loggerService.logError(error, 'QueueMannagerService');
+				}, () => {
+					this.unsubcribeConnection(connection.channel);
+					this.loggerService.logInfo('disconnected ' + connection.channel , 'QueueMannagerService');
+				});
+			}
+		});
 	}
 
 	public unsubcribeConnection(channel: number): void {
-		const connection: Connection = this.getConnectionByChannel(channel);
-		if (connection.isConnected && connection.subscription) {
-			connection.isConnected = false;
-			connection.subscription.unsubscribe();
-			connection.subscription = null;
-			clearInterval(connection.sendQueueProcessInterval);
-			clearInterval(connection.recivedQueueProcessInterval);
-			connection.sendQueueProcessInterval = null;
-		}
+		this.getConnectionByChannel(channel).then(connection => {
+			if (connection.isConnected && connection.subscription) {
+				connection.isConnected = false;
+				connection.subscription.unsubscribe();
+				connection.subscription = null;
+				clearInterval(connection.sendQueueProcessInterval);
+				clearInterval(connection.recivedQueueProcessInterval);
+				connection.sendQueueProcessInterval = null;
+			}
+		});
 	}
 
 	public addMessageToQueue(data: any): void {
-		const connection: Connection = this.getConnectionByChannel(data.channel);
-		this.enQueueMessage(data.data, connection.sendMessageQueue);
-		if (!connection.isConnected) {
-			this.connect(data);
-		}
+		this.getConnectionByChannel(data.channel).then(connection => {
+			this.enQueueMessage(data.data, connection.sendMessageQueue);
+			if (!connection.isConnected) {
+				this.connect(data);
+			}
+		});
 	}
 }
