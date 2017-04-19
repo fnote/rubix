e04327dca0eb8c4faeaa576f5e8fa0537f366c07
Amila Manathunga <amila@Amilas-MacBook-Pro.local>
2017-04-19 15:28:58 +0530

    Few UI enhancements

--- src/app/layouts/primary-layout-one/primary-layout-one.component.ts
+++ src/app/layouts/primary-layout-one/primary-layout-one.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
 	templateUrl: './primary-layout-one.component.html',
 })
 export class PrimaryLayoutOneComponent {
-	public navExpandClicked(): void {
+	public navExpandClicked(event: any): void {
+		event.preventDefault();
 		const body = document.getElementsByTagName('body')[0];
 		body.classList.toggle('nav-expanded');
 	}
