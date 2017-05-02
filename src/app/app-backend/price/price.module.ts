468ed036816bb7e12320123d8c061c26664ecb56
MalinduA <a.malindu@mubasher.net>
2017-05-02 19:25:53 +0530

    Small issue fix

--- src/app/app-backend/price/price.module.ts
+++ src/app/app-backend/price/price.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { DepthDataStore } from './data-stores/depth-data-store';
+import { ExchangeDataStore } from './data-stores/exchange-data-store';
 import { NgModule } from '@angular/core';
 import { PriceService } from './price.service';
 import { PriceStreamingResponseHandler } from './protocols/streaming/price-streaming-response-handler';
 import { PriceSubscriptionService } from './price-subscription.service';
+import { StockDataStore } from './data-stores/stock-data-store';
 import { TimeAndSalesDataStore } from './data-stores/time-and-sales-data-store';
 
 @NgModule({
@@ -15,6 +17,8 @@ import { TimeAndSalesDataStore } from './data-stores/time-and-sales-data-store';
 		PriceSubscriptionService,
 		DepthDataStore,
 		TimeAndSalesDataStore,
+		StockDataStore,
+		ExchangeDataStore,
 	],
 })
 export class PriceModule { }
