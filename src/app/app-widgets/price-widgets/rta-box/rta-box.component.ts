903c871d3f364307f5c15e9afff53e3d0c8ae72b
chandana <b.chandana@mubasher.net>
2017-06-22 12:19:00 +0530

    Add missing scss file for rta-box component

--- src/app/app-widgets/price-widgets/rta-box/rta-box.component.ts
+++ src/app/app-widgets/price-widgets/rta-box/rta-box.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
 	selector: 'app-rta-box',
 	templateUrl: './rta-box.component.html',
+	styleUrls: ['./rta-box.component.scss'],
 })
 export class RtaBoxComponent implements OnInit {
 
