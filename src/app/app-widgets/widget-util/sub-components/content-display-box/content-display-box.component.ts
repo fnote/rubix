97fc6ff705673a194f3fb9bc37d6e8a1b88df3f6
chandana <b.chandana@mubasher.net>
2017-06-22 15:08:19 +0530

    Add widget util component

---
+++ src/app/app-widgets/widget-util/sub-components/content-display-box/content-display-box.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-content-display-box',
+	templateUrl: './content-display-box.component.html',
+	styleUrls: ['./content-display-box.component.scss'],
+})
+export class ContentDisplayBoxComponent implements OnInit {
+
+	constructor() {
+		// code
+	}
+
+	public ngOnInit(): void {
+		// code
+	}
+
+}
