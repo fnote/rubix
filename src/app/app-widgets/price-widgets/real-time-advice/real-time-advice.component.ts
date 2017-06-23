fe6a81839b7d8ad20e796e8aaefcdd81d5ffa789
chandana <b.chandana@mubasher.net>
2017-06-23 14:24:41 +0530

    content-display-box component changes

--- src/app/app-widgets/price-widgets/real-time-advice/real-time-advice.component.ts
+++ src/app/app-widgets/price-widgets/real-time-advice/real-time-advice.component.ts
@@ -9,6 +9,17 @@ import { PriceService } from '../../../app-backend/price/price.service';
 })
 export class RealTimeAdviceComponent implements OnInit {
 
+	public contentData = [
+		{ imageUrl: 'http://placehold.it/120x120&text=image4',
+			title: 'Test Title1', discription: 'sample description1', fotterDis: '1 days ago', action1: 'BUY', action2: 'SELL' },
+		{ imageUrl: '', title: 'Test Title2', discription: 'this is a sample description2', fotterDis: '2 days ago', action1: 'BUY' },
+		{ imageUrl: '', title: 'Test Title3', discription: 'this is a sample description3', fotterDis: '3 days ago', action2: 'SELL' },
+		{ imageUrl: '', title: 'Test Title4', discription: 'this is a sample description4', action1: 'BUY', action2: 'SELL' },
+		{ imageUrl: '', title: 'Test Title5', discription: 'this is a sample description5', action1: 'BUY', action2: 'SELL' },
+		{ imageUrl: '', title: 'Test Title6', discription: 'this is a sample description6', fotterDis: '4 days ago' },
+		{ imageUrl: '', discription: 'this is a sample description7', fotterDis: '5 days ago' },
+	];
+
 	constructor(private priceService: PriceService) {}
 
 	public ngOnInit(): void {
