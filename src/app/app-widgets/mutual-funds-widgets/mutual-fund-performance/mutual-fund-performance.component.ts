8518c17d5d31aa9410a3ce7140a8dd20afee2377
Amila Manathunga <amila@Amilas-MacBook-Pro.local>
2017-06-27 09:13:17 +0530

    Added a C3 chart temperory until the proper chart is implemented

--- src/app/app-widgets/mutual-funds-widgets/mutual-fund-performance/mutual-fund-performance.component.ts
+++ src/app/app-widgets/mutual-funds-widgets/mutual-fund-performance/mutual-fund-performance.component.ts
@@ -1,15 +1,42 @@
-import { Component/**, OnInit*/ } from '@angular/core';
+import * as c3 from 'c3';
+import { BaseWidgetComponent } from '../../widget-util/base-widget/base-widget.component';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-mutual-fund-performance',
 	templateUrl: './mutual-fund-performance.component.html',
 	styleUrls: ['./mutual-fund-performance.component.scss'],
 })
-export class MutualFundPerformanceComponent { // implements OnInit {
+export class MutualFundPerformanceComponent extends BaseWidgetComponent {
 
+	// TODO: [Amila] Refactor the code once Malindu implement the chart properly
 	// constructor() { }
 
-	// ngOnInit() {
-	// }
+	public onInit(): void {
+		this.drawAreaChart();
+	}
 
+	private drawAreaChart(): void {
+		const areaChart = c3.generate({
+			bindto: '#c3Chart',
+			legend: {
+				show: false,
+			},
+			data: {
+				/* tslint:disable */
+				columns: [
+					['data1', 30, 200, 100, 400, 150, 250],
+					['data2', 130, 100, 140, 200, 150, 50],
+					['data3', 130, 150, 200, 300, 200, 100],
+				],
+				/* tslint:enable */
+				type: 'bar',
+			},
+			bar: {
+				width: {
+					ratio: 0.5, // this makes bar width 50% of length between ticks
+				},
+			},
+		});
+	}
 }
