0306681166a94199539eb5ef3f024a657db85540
chandana <b.chandana@mubasher.net>
2017-06-29 14:57:39 +0530

    Query mutual fund Items By Region

--- src/app/app-widgets/mutual-funds-widgets/mutual-fund-category/mutual-fund-category.component.ts
+++ src/app/app-widgets/mutual-funds-widgets/mutual-fund-category/mutual-fund-category.component.ts
@@ -1,5 +1,7 @@
 import { Component, Injector } from '@angular/core';
 import { BaseWidgetComponent } from '../../widget-util/base-widget/base-widget.component';
+import { MutualFundsDataStore } from '../../../app-backend/price/data-stores/mutual-funds-data-store';
+import { StrategyTypes } from '../../../app-constants/enums/strategy-typs.enum';
 
 @Component({
 	selector: 'app-mutual-fund-category',
@@ -16,22 +18,36 @@ export class MutualFundCategoryComponent extends BaseWidgetComponent {
 		{ imageUrl: 'img/img_four.png', title: 'Growth Strategy', discription: 'For Aggressive investors who wish to Sharia Compliant mutual funds' },
 		{ imageUrl: 'img/img_five.png', title: 'Growth Strategy', discription: 'For Aggressive investors who wish to Sharia Compliant mutual funds' },
 	];
+	public mutualFundCategoryObj: Object;
+	private _strategy: {type: number};
 
-	private _strategy: Object;
-
-	constructor(private injector: Injector) {
+	constructor(private injector: Injector, private mutualFundsDataStore: MutualFundsDataStore) {
 		super(injector);
 	}
 
-	public get strategy(): Object {
+	public onInit(): void {
+		let region: string;
+		switch (this._strategy.type) {
+			case StrategyTypes.Global:
+				region = 'Global';
+				break;
+			case StrategyTypes.Regional:
+				region = 'Regional';
+				break;
+			case StrategyTypes.ShariaCompliant:
+				region = 'Sharia';
+				break;
+			default:
+				region = '';
+		}
+		this.mutualFundCategoryObj = this.mutualFundsDataStore.getItemsByRegion(region);
+	}
+
+	public get strategy(): {type: number} {
 		return this._strategy;
 	}
 
-	public set strategy(value: Object) {
+	public set strategy(value: {type: number}) {
 		this._strategy = value;
 	}
-
-	// ngOnInit() {
-	// }
-
 }
