d01f3040985004735df9ccd32d3d106c22fa04ef
chandana <b.chandana@mubasher.net>
2017-06-29 20:07:19 +0530

    Content display box component changes

--- src/app/app-widgets/widget-util/sub-components/content-display-box/content-display-box.component.ts
+++ src/app/app-widgets/widget-util/sub-components/content-display-box/content-display-box.component.ts
@@ -9,9 +9,13 @@ export class ContentDisplayBoxComponent implements OnInit {
 	@Input() public contentData: Object;
 	@Input() public imageUrl: string;
 	@Input() public title: string;
-	@Input() public discription: string;
-	@Input() public fotterDis: string;
+	@Input() public headerClass: string;
+	@Input() public bodyClass: string;
+	@Input() public description: string;
+	@Input() public fullDescription: string;
+	@Input() public footerDes: string;
 	@Input() public actions: Array<number>;
+	@Input() public content: Object;
 	@Output() public testEventEmitter: EventEmitter<string> = new EventEmitter();
 
 	constructor() {
