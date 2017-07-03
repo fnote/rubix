46dcbf516ba3e2a95f48581ec7d2f8ce4fe0233b
Amila Manathunga <amila@Amilas-MacBook-Pro.local>
2017-07-03 16:45:11 +0530

    Code refactoring

--- src/app/app-widgets/widget-util/sub-components/content-display-box/content-display-box.component.ts
+++ src/app/app-widgets/widget-util/sub-components/content-display-box/content-display-box.component.ts
@@ -1,11 +1,10 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
 	selector: 'app-content-display-box',
 	templateUrl: './content-display-box.component.html',
-	styleUrls: ['./content-display-box.component.scss'],
 })
-export class ContentDisplayBoxComponent implements OnInit {
+export class ContentDisplayBoxComponent {
 	@Input() public contentData: Object;
 	@Input() public imageUrl: string;
 	@Input() public title: string;
@@ -17,14 +16,6 @@ export class ContentDisplayBoxComponent implements OnInit {
 	@Input() public actions: Array<number>;
 	@Input() public content: Object;
 	@Output() public testEventEmitter: EventEmitter<string> = new EventEmitter();
-
-	constructor() {
-		// code
-	}
-
-	public ngOnInit(): void {
-		// code
-	}
 
 	public onActionBubble(event: Event, sampleString: string): void {
 		event.stopPropagation();
